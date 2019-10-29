7feb62e246096efc7545f4a4f0fadb16ae50d535
wtfan <fanweitian@yuewen.com>
2019-10-29 15:10:58 +0800

    fix：修正快速插入和删除时，offset非ScrollPositionWithSingleContext，导致contentSize会设置为0。

--- lib/src/extended_editable_text.dart
+++ lib/src/extended_editable_text.dart
@@ -1534,11 +1534,13 @@ class ExtendedEditableTextState extends State<ExtendedEditableText>
         if (offset != null && offset is ScrollPosition) {
           var sp = offset as ScrollPosition;
           if (sp.minScrollExtent != null && sp.maxScrollExtent != null) {
-            //pixels should >= minScrollExtent
-            //pixels should <= maxScrollExtent
-            if (sp.pixels < sp.minScrollExtent ||
-                sp.pixels > sp.maxScrollExtent) {
-              offset = ViewportOffset.zero();
+            // pixels should >= minScrollExtent
+            // pixels should <= maxScrollExtent
+            if (sp.pixels < sp.minScrollExtent) {
+              sp.correctPixels(sp.minScrollExtent);
+            }
+            if (sp.pixels > sp.maxScrollExtent) {
+              sp.correctPixels(sp.maxScrollExtent);
             }
           }
         }
